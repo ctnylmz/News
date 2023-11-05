4860f42db6082ef64b96d1bfbc1bb18610e40cf5
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2023-11-05 13:49:40 +0300

    7 Days

--- MyFinalProject/ConsoleUI/Program.cs
+++ MyFinalProject/ConsoleUI/Program.cs
@@ -1,2 +1,10 @@
 ﻿
-Console.WriteLine("Hello, World!");
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+foreach (var product in productManager.GetAll())
+{
+    Console.WriteLine(product.ProductName);
+}
