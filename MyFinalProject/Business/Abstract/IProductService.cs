e76cd0cb2a8c356ff87658f2c47bf212c84f16fd
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2023-11-12 12:25:50 +0300

    DTO Get Product Details

--- MyFinalProject/Business/Abstract/IProductService.cs
+++ MyFinalProject/Business/Abstract/IProductService.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Concrete;
+using Entities.DTOs;
 using System.Collections.Generic;
 
 
@@ -10,6 +11,7 @@ namespace Business.Abstract
         List<Product> GetAllByCategoryId(int id);
 
         List<Product> GetAllByUnitPrice(decimal min, decimal max);
+        List<ProductDetailDto> GetProductDetails();
 
     }
 }
--- MyFinalProject/ConsoleUI/Program.cs
+++ MyFinalProject/ConsoleUI/Program.cs
@@ -8,6 +8,11 @@ public class Program
     public static void Main(string[] args)
     {
 
+        ProductTest();
+    }
+
+    private static void CategoryTest()
+    {
         CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
 
         foreach (var category in categoryManager.GetAll())
@@ -15,14 +20,13 @@ public class Program
             Console.WriteLine(category.CategoryName);
         }
     }
-
-    private static void ProductTest()
+        private static void ProductTest()
     {
         ProductManager productManager = new ProductManager(new EfProductDal());
 
-        foreach (var product in productManager.GetAllByUnitPrice(50, 100))
+        foreach (var product in productManager.GetProductDetails())
         {
-            Console.WriteLine(product.ProductName);
+            Console.WriteLine(product.ProductName + "/" + product.CategoryName);
         }
 
     }
