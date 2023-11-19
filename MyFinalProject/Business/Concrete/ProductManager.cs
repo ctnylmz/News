211df11a3bb77e464023af3ece3a87de56bb179e
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2023-11-19 13:02:50 +0300

    Create Validation

--- MyFinalProject/Business/Concrete/ProductManager.cs
+++ MyFinalProject/Business/Concrete/ProductManager.cs
@@ -1,10 +1,14 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
+using System.ComponentModel.DataAnnotations;
 
 namespace Business.Concrete
 {
@@ -19,10 +23,9 @@ namespace Business.Concrete
 
         public IResult Add(Product product)
         {
-            if (product.ProductName.Length<2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+
+            ValidationTool.Validate(new ProductValidator(),product);
+
             _productDal.Add(product);
 
             return new SuccessResult(Messages.ProductAdded);
