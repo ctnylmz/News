c15da67eea213641399055e59e272654b250cebd
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2023-12-08 12:22:36 +0300

    Create CategoryAll

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -28,7 +28,7 @@ namespace WebAPI.Controllers
         public IActionResult GetAll()
         {
           
-            Thread.Sleep(5000);
+            Thread.Sleep(1000);
 
             var result =  _productService.GetAll();
             if (result.Success)
@@ -50,6 +50,20 @@ namespace WebAPI.Controllers
 
             return BadRequest(result);
         }
+
+
+        [HttpGet("getbycategory/{categoryId}")]
+        public IActionResult GetByCategory(int categoryId)
+        {
+            var result = _productService.GetAllByCategoryId(categoryId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
 
         [HttpPost("add")]
         public IActionResult Add(Product product)
